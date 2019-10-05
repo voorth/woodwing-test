5388b01ca42edb90362315d042aac77c62d0e467
henk.voorthuijsen <henk.voorthuijsen@eonics.nl>
2019-10-05 13:38:19 +0200

    First commit.

---
+++ src/test/java/nl/voorth/woodwing/SimpleTest.java
@@ -0,0 +1,14 @@
+package nl.voorth.woodwing;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+public class SimpleTest
+{
+  @Test
+  public void testTest() {
+    fail("failure");
+  }
+}
---
+++ src/test/java/nl/voorth/woodwing/WoodwingTestApplicationTests.java
@@ -0,0 +1,20 @@
+package nl.voorth.woodwing;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class WoodwingTestApplicationTests
+{
+
+  @Test
+  public void contextLoads()
+  {
+    Assertions.fail("failure");
+  }
+
+}
